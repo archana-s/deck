0abf29053dee39b4209c4264b0e3cac3f508ba68
Archana Sankaranarayanan <asankar@netflix.com>
2017-08-18 18:15:30 -0700

    Removing old files

--- app/scripts/modules/core/styleguide/styleguide.module.ts
+++
@@ -1,8 +0,0 @@
-const angular = require('angular')
-
-import {STYLEGUIDE_STATES} from './styleguide.states';
-
-angular
-  .module('spinnaker.core.styleguide', [STYLEGUIDE_STATES]);
-
-export const STYLEGUIDE_MODULE = 'spinnaker.core.styleguide';
